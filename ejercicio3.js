9a985aca2e824f68478674a70c67a3e005285b01
rjimenez <r.jimenez.2021@alumnos.urjc.es>
2024-10-05 13:10:30 +0200

    ej 2 pero modificando el cursor

---
+++ ejercicio3.js
@@ -0,0 +1,19 @@
+AFRAME.registerComponent('crear-cubo', {
+  
+    init: function () {
+        var entity = this.el;
+        //var sceneEl = this.sceneEl;
+        entity.addEventListener('click', function () {
+            console.log('click esfera');
+            var cuboNuevo = document.createElement('a-entity');
+            cuboNuevo.setAttribute('geometry', {
+                primitive: 'box',
+                height: 1,
+                width: 1
+            });
+            cuboNuevo.setAttribute('material', 'color', 'red');
+            cuboNuevo.setAttribute('position', '0 1.5 -3');
+            this.sceneEl.appendChild(cuboNuevo);
+        });
+    }
+});
