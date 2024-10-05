d516c807d64aa86414121ab838bf7582b0c94688
rjimenez <r.jimenez.2021@alumnos.urjc.es>
2024-10-05 21:45:58 +0200

    Creacion de figuras apiladas

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,26 +1,32 @@
 AFRAME.registerComponent('crear-entidad', {
     init: function () {
       var entity = this.el;
+      var childCount = 0;
+  
       entity.addEventListener('click', function () {
-        console.log('click entidad');
-        var newEntity = entity.cloneNode(true);
+        var newEntity = document.createElement('a-entity');
         
-        // Obtener la posición actual de la entidad
-        var currentPosition = entity.getAttribute('position');
-      
-        // Calcular la nueva posición relativa a la entidad actual
-        var newX = currentPosition.x + 2;
-        var newY = currentPosition.y- 2;
-        var newZ = currentPosition.z + 2;
-      
+        var geometry = entity.getAttribute('geometry');
+        var material = entity.getAttribute('material');
+        newEntity.setAttribute('geometry', geometry);
+        newEntity.setAttribute('material', material);
+        // Obtener la posición de la entidad padre
+        //var parentPosition = entity.getAttribute('position');
+        //var parentPosition = entity.object3D.position;
+        var parentWorldPosition = new THREE.Vector3();
+        entity.object3D.getWorldPosition(parentWorldPosition);
+        
+        var newX = parentWorldPosition.x;
+        var newY = parentWorldPosition.y + (childCount+2); // Siempre añade 2 unidades a la altura
+        var newZ = parentWorldPosition.z;
+        
         newEntity.setAttribute('position', `${newX} ${newY} ${newZ}`);
         
-        // Eliminar el componente crear-entidad del clon para evitar creación infinita
-        newEntity.removeAttribute('crear-entidad');
+        entity.sceneEl.appendChild(newEntity);
         
-        // Añadir la nueva entidad a la entidad clickeada
-        entity.appendChild(newEntity);
+        childCount++;
       });
     }
   });
   
+   
